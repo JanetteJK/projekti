a686527464aa4d7ac0cca2757621f50d06a8a9a7
LTaskinen <lauri.taskinen2@metropolia.fi>
2025-03-06 10:10:25 +0200

    .

--- PROJEKTIRYHMÄ6/Tietokantayhteys.py
+++ PROJEKTIRYHMÄ6/Tietokantayhteys.py
@@ -2,7 +2,8 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
+        host="127.0.0.1",
+        port=3306,
         user="lauri1",
         password="Kukkokiekuu1",
         database="flight_game",
@@ -16,3 +17,11 @@ def get_persons ():
         persons = cursor.fetchall()
         conn.close()
         return persons
+
+get_persons()
+def uusi_asiakas(nimi):
+    person= f'SELECT ID, nimi, difficulty from person where nimi={nimi}'
+    print(person)
+    return
+uusi_asiakas('Pauli Pomottelija')
+
