667d218a65ce589aea2901e02f4df4c9edb9ab28
LTaskinen <lauri.taskinen2@metropolia.fi>
2025-03-06 11:23:51 +0200

    .

--- PROJEKTIRYHMÄ6/Asiakas_Funktio.py
+++ PROJEKTIRYHMÄ6/Asiakas_Funktio.py
@@ -1,1 +1,12 @@
-def uusi_asiakas():
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="lauri1",
+        password="Kukkokiekuu1",
+        database="flight_game",
+        collation='latin1_swedish_ci',
+        autocommit=True
+    )
--- PROJEKTIRYHMÄ6/PELI.py
+++ PROJEKTIRYHMÄ6/PELI.py
@@ -1,15 +1,12 @@
-#Tänne kaikki funktion kutsunnat
 import mysql.connector
 
-
-connection = mysql.connector.connect(
-    host='localhost',
-    port=3306,
-    database='flight_game',
-    user='lauri1',
-    password='Kukkokiekuu1',
-    autocommit=True
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="lauri1",
+        password="Kukkokiekuu1",
+        database="flight_game",
+        collation='latin1_swedish_ci',
+        autocommit=True
     )
-
-
-def main():
