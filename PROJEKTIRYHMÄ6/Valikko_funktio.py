123dea2f386435c47d0165fb1287fed5c2d3aaa5
julijavan <juli.javanainen@metropolia.fi>
2025-03-11 14:34:10 +0200

    julin säätämistä valikkoon

--- PROJEKTIRYHMÄ6/Valikko_funktio.py
+++ PROJEKTIRYHMÄ6/Valikko_funktio.py
@@ -9,33 +9,34 @@ import mysql.connector
 from Tietokantayhteys import get_db_connection
 import random
 
-
 def valikko_kohteet():
     try:
         conn = get_db_connection()
         cursor = conn.cursor(dictionary=True)
 
-        # onks tää yhteys oikein?
-        # tähän pitää myös vielä lisätä et hakee sen yhden oikeen vastauksen sekaan
+        # Select only the name from country table
         cursor.execute("SELECT name FROM country ORDER BY RAND() LIMIT 9")
         countries = cursor.fetchall()
 
-        country_ids = [country['country_id'] for country in countries]
+        country_names = [country['name'] for country in countries]
 
-        format_strings = ','.join(['%s'] * len(country_ids))
+        # käytetään maan nimi instead of country_id myöhemmin
+        format_strings = ','.join(['%s'] * len(country_names))
         cursor.execute(
-            f"SELECT person_id, answer, correct FROM answer WHERE country_id IN ({format_strings})",
-            country_ids)
+            f"SELECT country.name AS country_name, person_id, answer, correct FROM answer "
+            f"JOIN country ON answer.country_id = country.id "
+            f"WHERE country.name IN ({format_strings})",
+            country_names)
         answers = cursor.fetchall()
 
         results = []
-        for country_id in country_ids:
-            matching_answers = [a for a in answers if a.get('country_id') == country_id]
+        for country_name in country_names:
+            matching_answers = [a for a in answers if a.get('country_name') == country_name]
             answer_info = matching_answers[0] if matching_answers else {"person_id": None, "answer": None,
                                                                         "correct": None}
 
             results.append({
-                "country_id": country_id,
+                "country_name": country_name,
                 "person_id": answer_info["person_id"],
                 "answer": answer_info["answer"],
                 "correct": answer_info["correct"]
