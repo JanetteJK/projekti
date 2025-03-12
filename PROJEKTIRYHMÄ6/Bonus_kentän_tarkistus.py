5843f94f0b98555ebfb310e74772f6d8be06fb45
LTaskinen <lauri.taskinen2@metropolia.fi>
2025-03-12 12:26:27 +0200

    .

--- PROJEKTIRYHMÄ6/Bonus_kentän_tarkistus.py
+++ PROJEKTIRYHMÄ6/Bonus_kentän_tarkistus.py
@@ -1,5 +1,10 @@
 #jos sinulla on 100+ rahaa saat bonuskentän
+from Rahan_Lisäys import Rahapussi
+
+rahapussi = Rahapussi()
+
 def bonus_tarkistus():
-    rahat = palkka()
+    rahat = rahapussi.hae_saldo()
     if rahat >= 100:
-        return true
+        person_id = 11
+        order_no = 1
